eb03b7ae5c1335437547aaf45e05778b30a75d5c
juergens <juergens@TOWER>
2015-11-08 11:29:55 +0100

    show the use of Roaster in test

--- src/test/java/net/hjuergens/freemarker/maven/plugin/FreeMarkerMojoTest.java
+++ src/test/java/net/hjuergens/freemarker/maven/plugin/FreeMarkerMojoTest.java
@@ -3,51 +3,19 @@
 package net.hjuergens.freemarker.maven.plugin;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.jboss.forge.roaster.Roaster;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jboss.forge.roaster.model.source.JavaDocSource;
 
 import java.io.File;
+import java.io.FileWriter;
 
 /**
- *     <directory>${project.basedir}/target</directory>
-    <outputDirectory>${project.build.directory}/classes</outputDirectory>
-    <finalName>${project.artifactId}-${project.version}</finalName>
-    <testOutputDirectory>${project.build.directory}/test-classes</testOutputDirectory>
-    <sourceDirectory>${project.basedir}/src/main/java</sourceDirectory>
-    <scriptSourceDirectory>src/main/scripts</scriptSourceDirectory>
-    <testSourceDirectory>${project.basedir}/src/test/java</testSourceDirectory>
-    <resources>
-      <resource>
-        <directory>${project.basedir}/src/main/resources</directory>
-      </resource>
-    </resources>
-    <testResources>
-      <testResource>
-        <directory>${project.basedir}/src/test/resources</directory>
-      </testResource>
-    </testResources>
-    * 
  * @author juergens
  */
 public class FreeMarkerMojoTest
     extends AbstractMojoTestCase
 {
-    /** {@inheritDoc} */
-    @Override
-    protected void setUp()
-        throws Exception
-    {
-        // required
-        super.setUp();
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    protected void tearDown()
-        throws Exception
-    {
-        // required
-        super.tearDown();
-    }
-
     /**
      * @throws Exception if any
      */
@@ -61,6 +29,16 @@ public class FreeMarkerMojoTest
         FreeMarkerMojo myMojo = (FreeMarkerMojo) lookupMojo( "process", pom );
         assertNotNull( myMojo );
         myMojo.execute();
+
+        File srcFile = new File("target/generated-test-sources/project01/java/Database.java");
+        final JavaClassSource javaClass =Roaster.parse(JavaClassSource.class, srcFile);
+        assertEquals("Database", javaClass.getName());
+        JavaDocSource javaDoc = javaClass.getJavaDoc();
+        javaDoc.addTagValue("@author","FreeMarkerMojo");
+        String formatted = Roaster.format( javaClass.toString() );
+        FileWriter writer = new FileWriter(srcFile);
+        writer.write(formatted);
+        writer.close();
     }
 
     /**
